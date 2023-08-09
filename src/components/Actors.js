647480ad731f101571dfc113a3e7583b94ab518b
Tiffany Mwangi <tiffany.mwagi@student.moringaschool.com>
2023-08-09 15:43:48 +0300

    completed assignmnet

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,22 @@
-import React from "react";
-import { actors } from "../data";
+import React from 'react';
+import { actors } from '../data'; // Assuming you have actor data imported
 
-function Actors() {
-  return <div>{/*{code here}*/}</div>;
-}
+const Actors = () => {
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Actors;
